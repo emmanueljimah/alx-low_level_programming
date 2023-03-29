cdae587348d45de7539bcebccb67071efd7b12fa
emmanueljimah <b.emma.j38@gmail.com>
2023-03-29 08:11:10 -0700

    betty  char updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - C program that prints the size of various types on the computer it is compiled
+ * main - C program that prints the size of various types
  * Return: 0 (success)
  */
 int main(void)
@@ -10,7 +10,6 @@ int b;
 long int c;
 long long int d;
 float f;
-
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
